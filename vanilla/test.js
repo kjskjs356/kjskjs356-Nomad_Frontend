5d8d9896d2e3e7364a65ff68e779b623e835fbfb
kjskjs356 <kjskjs356@gmail.com>
2023-01-10 11:07:29 +0900

    study 0110

--- vanilla/test.js
+++ vanilla/test.js
@@ -1,2 +1,15 @@
-const a = 5;
-console.log(a);
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
